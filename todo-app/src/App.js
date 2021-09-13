3ac1369e5ef5de35bde3e86f685691e47e0f8125
YeongJun Ahn <53427147+dudwns9331@users.noreply.github.com>
2021-09-13 23:05:24 +0900

    Project Sample : todo-app(#4)

---
+++ todo-app/src/App.js
@@ -0,0 +1,57 @@
+import React, { useCallback, useRef, useState } from 'react';
+import TodoInsert from 'components/Todo/TodoInsert';
+import TodoList from 'components/Todo/TodoList';
+import TodoTemplate from 'components/Todo/TodoTemplate';
+
+// 많은 데이터 렌더링하기
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  // useState(함수이름) 을 통해서 처음 렌더링 시에 createBulkTodos가 실행되게 한다.
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1; // nextId 1씩 더하기
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
+
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
---
+++ todo-app/src/components/Todo/TodoListItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+
+import cn from 'classnames';
+import 'components/Scss/TodoListItem.scss';
+
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
+  const { id, text, checked } = todo;
+
+  return (
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// React.memo로 감싸줌으로써 todo, onRemove, onToggle이 바뀌지 않으면 리렌더링을 하지 않는다.
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
